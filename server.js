32b7b55f1361a2bc58d1632142e7b8cea915e24c
Juonc <287449677@qq.com>
2021-09-12 05:15:10 +1000

    v15

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ var http = require('http').Server(app)
 var mongoose = require('mongoose');
 const { send } = require('process')
 
-///const port = process.env.port || 3010;
-var port_number = server.listen(process.env.PORT || 3010);
-app.listen(port_number);
+const port = process.env.port || 3010;
+app = express()
+app.set("port", PORT)
+//var port_number = server.listen(process.env.PORT || 3010);
+//app.listen(port_number);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
